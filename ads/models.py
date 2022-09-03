f90fc01dd9053bcfd347ca9a029dab0c1a0e2cb3
VladimirMonin <79788260286@yandex.ru>
2022-09-03 17:06:24 +0600

    feat: models is done. Converting to json is updated!

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Ads(models.Model):
+    name = models.TextField(max_length=100)
+    author = models.TextField(max_length=100)
+    price = models.IntegerField()
+    description = models.TextField(max_length=1000)
+    address = models.TextField(max_length=200)
+    is_published = models.BooleanField()
+
+
+class Categories(models.Model):
+    name = models.TextField(max_length=100)
---
+++ utils.py
@@ -0,0 +1,55 @@
+import csv
+import json
+
+JSON_FILE1 = r'./data/ads.json'
+CSV_FILE1 = r'./raw_data/ads.csv'
+
+JSON_FILE2 = r'./data/categories.json'
+CSV_FILE2 = r'./raw_data/categories.csv'
+
+
+def clean_csv_data(data_list: list[dict]) -> list[dict]:
+    """
+    Превращает строковые значения ID в целочисленные, так же приводит ключи Id и id к pk.
+    Приводит строковые true false к булевым
+    Приводит цены из строк к целочисленным
+    :param data_list: Список данных из csv
+    :return: list
+    """
+    for row in data_list:
+        # Причесали ключи
+        if 'Id' in row.keys():
+            row['pk'] = int(row.pop('Id'))
+        if 'id' in row.keys():
+            row['pk'] = int(row.pop('id'))
+        # Причесали булевы значения
+        if 'is_published' in row.keys():
+            if row['is_published'] == 'TRUE':
+                row['is_published'] = True
+            else:
+                row['is_published'] = False
+        # Причесали цены
+        if 'price' in row.keys():
+            row['price'] = int(row['price'])
+
+    return data_list
+
+
+def csv_to_json(csv_file, json_file):
+    """
+    Принимает адрес csv и отдает json файл. Использует функцию "очистки" данных
+    :param csv_file: csv_file path
+    :param json_file: json_file path
+    :return: rewrite json_file
+    """
+    with open(csv_file, encoding='utf-8') as csvfile:
+        csv_row = csv.DictReader(csvfile)
+        data_list = [row for row in csv_row]
+        data_list = clean_csv_data(data_list)
+
+    with open(json_file, 'w', encoding='utf-8') as jsonfile:
+        jsonfile.write(json.dumps(data_list, indent=4, ensure_ascii=False))
+
+
+csv_to_json(CSV_FILE1, JSON_FILE1)
+csv_to_json(CSV_FILE2, JSON_FILE2)
