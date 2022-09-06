51e1af32a03b72c4992cd27ae49a084719bcdd0e
VladimirMonin <79788260286@yandex.ru>
2022-09-06 15:08:59 +0600

    feat: add CatDetailView

--- ads/views.py
+++ ads/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.generic import DetailView
+from django.views.generic.detail import BaseDetailView
 
-from ads.models import Ad
+from ads.models import Ad, Categories
 
 
 def response_200(request):
@@ -24,5 +25,19 @@ class AdDetailView(DetailView):
                 'description': ad.description,
                 'address': ad.address,
                 'is_published': ad.is_published
+            }
+            , safe=False, json_dumps_params={'ensure_ascii': False})
+
+
+class CatDetailView(DetailView):
+    model = Categories
+
+    def get(self, request, *args, **kwargs):
+        cat = self.get_object()
+
+        return JsonResponse(
+            {
+            'id': cat.pk,
+            'name': cat.name
             }
             , safe=False, json_dumps_params={'ensure_ascii': False})
